de16414561d9f79c4b46c0a1e43d3d0c4dea729f
Safwaniw <waleed.safwani@yahoo.com>
2019-07-26 17:19:40 +0300

    Import and httpClientModule -cart.service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class CartService {
   items = [];
+  
+  constructor(
+    private http: HttpClient
+  ) { }
 
   addToCart(product) {
     this.items.push(product);
@@ -16,6 +21,6 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  constructor() { }
+
 
 }
