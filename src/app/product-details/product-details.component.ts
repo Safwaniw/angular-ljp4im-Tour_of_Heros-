ae2d235d2205e7292e91c470c86d3a94fdca0030
Safwaniw <waleed.safwani@yahoo.com>
2019-07-26 14:22:09 +0300

    Inject CartService

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -11,7 +13,10 @@ import { products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product;
 
-  constructor(private route: ActivatedRoute, ) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
