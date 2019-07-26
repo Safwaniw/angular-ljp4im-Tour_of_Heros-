bb9d8f59cb0d01a37816ffa7813f1b2e9f843889
Safwaniw <waleed.safwani@yahoo.com>
2019-07-26 14:24:44 +0300

    Difining method addToCart

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -18,6 +18,10 @@ export class ProductDetailsComponent implements OnInit {
     private cartService: CartService
   ) { }
 
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')];
