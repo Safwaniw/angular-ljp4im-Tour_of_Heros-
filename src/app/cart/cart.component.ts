68cf028f4d9178451c2f860d54c97679135358d9
Safwaniw <waleed.safwani@yahoo.com>
2019-07-31 16:14:13 +0300

    adding onSubmit method -cart.component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -21,7 +21,15 @@ export class CartComponent implements OnInit {
       address: ''
     });
   }
-  
+
+  onSubmit(customerData) {
+    //Process checkout date here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+  }
 
   ngOnInit() {
   }
